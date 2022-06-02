d9389d1873a5a5e69d6c2ec7db6e14b137f7ff26
Preetam Raj <iitianpreetam@gmail.com>
2022-06-02 18:21:38 +0530

    updated whiteboard

--- code_together/routing.py
+++ code_together/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .consumers import CodeConsumer, ChatConsumer, UserConsumer
+from .consumers import CodeConsumer, ChatConsumer, UserConsumer, DrawConsumer
 
 websocket_urlpatterns = [
     path('ws/code/<str:room_id>/<str:username>/', CodeConsumer.as_asgi()),
     path('ws/chat/<str:room_id>/<str:username>/', ChatConsumer.as_asgi()),
     path('ws/user/<str:room_id>/<str:username>/', UserConsumer.as_asgi()),
+    path('ws/draw/<str:room_id>/<str:username>/', DrawConsumer.as_asgi()),
 ]
